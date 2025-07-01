901ee11f2eebce4ce4449a2d35388a2c68a03e7d
Lucky Goyal <122284578+goyalucky@users.noreply.github.com>
2025-07-01 08:51:33 +0530

    Delete ProgrammingElements directory

--- ProgrammingElements/Level1/Age.java
+++
@@ -1,8 +0,0 @@
-class Main{
-    public static void main(String[] args) {
-        int birth = 2000;
-        int curr = 2024;
-        int age = curr - birth;
-        System.out.println(" Harry's age in 2024 is" + age);
-    }
-}
--- ProgrammingElements/Level1/Marks.java
+++
@@ -1,9 +0,0 @@
-public class Marks {
-    public static void main(String[] args) {
-        int math = 94;
-        int Physics = 95;
-        int Chemistry = 96;
-        int res = (math + Physics + Chemistry)/3;
-        System.out.println("Sam’s average mark in PCM is" + res);
-    }
-}
